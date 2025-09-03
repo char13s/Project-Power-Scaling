ba267569165996290502d876a174912cf516f1cb
Charles Cunningham <32624173+char13s@users.noreply.github.com>
2025-09-02 23:56:27 -0400

    Added Item pick up the enter into inventory.

--- ProjectPowerScale/Assets/Scripts/Item  related scriptss/ItemObject.cs
+++ ProjectPowerScale/Assets/Scripts/Item  related scriptss/ItemObject.cs
@@ -1,6 +1,21 @@
 using UnityEngine;
-
+[RequireComponent(typeof(SphereCollider))]
+[RequireComponent(typeof(Rigidbody))]
 public class ItemObject : MonoBehaviour
 {
     [SerializeField] private Item itemData;
+    public Item ItemData { get => itemData; set => itemData = value; }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Assuming the player has an Inventory component with an AddItem method
+            Inventory playerInventory = other.GetComponent<Inventory>();
+            if (playerInventory != null)
+            {
+                playerInventory.AddItem(itemData,1);
+                Destroy(gameObject); // Remove the item from the scene after picking it up
+            }
+        }
+    }
 }
